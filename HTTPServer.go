891c6364dc227e5287e5faf19b9d3d3752cad7d2
Young.hedong <Young.hedong@gmail.com>
2017-10-28 21:22:05 +0800

    done

---
+++ HTTPServer.go
@@ -0,0 +1,72 @@
+/*
+Serve is a very simple static file server in go
+Usage:
+	-p="8100": port to serve on
+	-d=".":    the directory of static files to host
+Navigating to http://localhost:8100 will display the index.html or directory
+listing file.
+*/
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+    "net"
+    "errors"
+)
+
+//get local IP
+func externalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("are you connected to the network?")
+}
+
+func main() {
+	port := flag.String("p", "8100", "port to serve on")
+	directory := flag.String("d", ".", "the directory of static file to host")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir(*directory)))
+
+    ip, err := externalIP()
+    if err != nil {
+	    log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
+    } else {
+        log.Printf("Serving %s on %s:%s\n", *directory, ip, *port)
+    }
+
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HTTPServer
+静态文件下载服务器，解决测试Android App时需要一个一个用USB连接手机安装App，OPPO手机又那么傻每次开启USB调试要输入图形验证码
+
+usage:
+PATH_TO_HTTPSERVER/HTTPServer [-d the directory of static file to host, default .] [-p port to serve on, default 8100]
